eac4f41961e833b1a6790656c4ff46e7c068543d
Connor Thomson <blumatrikz@gmail.com>
2025-12-12 19:47:12 -0800

    Update gdt.h

--- src/kernel/gdt/gdt.h
+++ src/kernel/gdt/gdt.h
@@ -1,50 +1,6 @@
 #ifndef GDT_H
 #define GDT_H
 
-struct gdt_entry {
-    unsigned short limit_low;
-    unsigned short base_low;
-    unsigned char base_middle;
-    unsigned char access;
-    unsigned char granularity;
-    unsigned char base_high;
-} __attribute__((packed));
-
-struct gdt_ptr {
-    unsigned short limit;
-    unsigned int base;
-} __attribute__((packed));
-
-struct tss_entry {
-    unsigned int prev_tss;
-    unsigned int esp0;
-    unsigned int ss0;
-    unsigned int esp1;
-    unsigned int ss1;
-    unsigned int esp2;
-    unsigned int ss2;
-    unsigned int cr3;
-    unsigned int eip;
-    unsigned int eflags;
-    unsigned int eax;
-    unsigned int ecx;
-    unsigned int edx;
-    unsigned int ebx;
-    unsigned int esp;
-    unsigned int ebp;
-    unsigned int esi;
-    unsigned int edi;
-    unsigned int es;
-    unsigned int cs;
-    unsigned int ss;
-    unsigned int ds;
-    unsigned int fs;
-    unsigned int gs;
-    unsigned int ldt;
-    unsigned short trap;
-    unsigned short iomap_base;
-} __attribute__((packed));
-
 extern void gdt_flush(unsigned int gdt_ptr_address);
 extern void tss_flush(unsigned short selector);
 
