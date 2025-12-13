1c106335e5b41d82891610b9fc2f8aae4b85be34
Connor Thomson <blumatrikz@gmail.com>
2025-12-12 19:58:08 -0800

    Update syscalls.c

--- src/kernel/syscalls/syscalls.c
+++ src/kernel/syscalls/syscalls.c
@@ -1,5 +1,16 @@
-#include "vga.h"
+.intel_syntax noprefix
+.global syscall_handler
 
-void syscall_handler(void) {
-  print("Hello, syscalls!");
-}
+syscall_handler:
+    push rbp
+    mov rbp, rsp
+    
+    lea rdi, [rip + syscall_msg]
+    call print
+    
+    leave
+    ret
+
+.section .rodata
+syscall_msg:
+    .asciz "Hello, syscalls!"
