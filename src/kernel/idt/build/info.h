9317402d71f07bd8f424b7d85f69040765e92e01
Connor Thomson <blumatrikz@gmail.com>
2025-12-12 19:50:47 -0800

    Delete src/kernel/idt/build directory

--- src/kernel/idt/build/info.h
+++
@@ -1,8 +0,0 @@
-#ifndef INFO_H
-#define INFO_H
-#define CC "gcc 14.2.1"
-#define AS "GNU assembler 2.44"
-#define HOST "Linux 6.12.51_1"
-#define BUILD_TIME __TIME__
-#define BUILD_DATE __DATE__
-#endif
