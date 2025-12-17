ce43fc2f1c7bbcaed6ec6ffba63a308f93a47551
Connor Thomson <blumatrikz@gmail.com>
2025-12-17 15:40:34 -0800

    Update and rename kernel.S to kernel.asm

---
+++ src/kernel/kernel.asm
@@ -0,0 +1,41 @@
+global _start
+
+extern clear_screen
+extern panic
+extern gas_version
+extern build_date
+extern printk
+
+section .text
+_start:
+    call clear_screen
+
+    push one
+    call printk
+    add esp, 4
+
+    push gas_version
+    call printk
+    add esp, 4
+
+    push two
+    call printk
+    add esp, 4
+
+    push build_date
+    call printk
+    add esp, 4
+
+    push newline
+    call printk
+    add esp, 4
+
+    push string_panic_msg
+    call panic
+    add esp, 4
+
+section .rodata
+string_panic_msg db "Nothing else implemented (yet)", 10, 0
+one             db "CINUX (Built with: ", 0
+two             db ") Built on: ", 0
+newline         db 10, 0
