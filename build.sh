dd7fd3e24596e3aab31b027d356b0f5361e6f63d
Connor Thomson <blumatrikz@gmail.com>
2025-12-17 15:53:56 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-CC=gcc
-AS=as
+NASM=nasm
 LD=ld
 
 BUILD_DIR=build
@@ -28,33 +27,29 @@ all() {
     LDFLAGS="-m elf_i386 -T src/kernel/link.ld"
     USER_LDFLAGS="-m elf_i386 -T src/user/link.ld"
 
-    GAS_VER=$(as --version | head -n1 | sed 's/([^)]*) //g')
+    NASM_VER=$($NASM -v)
     BUILD_DATE=$(date)
 
-    cat > build_info.S <<EOF
-.intel_syntax noprefix
-.section .rodata
-.global gas_version
-.global build_date
+    cat > build_info.asm <<EOF
+global nasm_version
+global build_date
 
-gas_version:
-    .asciz "${GAS_VER}"
-
-build_date:
-    .asciz "${BUILD_DATE}"
+section .rodata
+nasm_version db "${NASM_VER}", 0
+build_date   db "${BUILD_DATE}", 0
 EOF
 
     CFILES=$(find . -name "*.c")
-    SFILES=$(find . -path ./src/boot -prune -o -name "*.S" -print)
+    ASMFILES=$(find . -path ./src/boot -prune -o -name "*.asm" -print)
 
     for f in $CFILES; do
         o="$BUILD_DIR/$(basename "${f%.*}").o"
-        $CC $CFLAGS -c "$f" -o "$o"
+        gcc $CFLAGS -c "$f" -o "$o"
     done
 
-    for f in $SFILES; do
+    for f in $ASMFILES; do
         o="$BUILD_DIR/$(basename "${f%.*}").o"
-        $AS --32 "$f" -o "$o"
+        $NASM -f elf32 "$f" -o "$o"
     done
 
     KERNEL_OBJS=$(find "$BUILD_DIR" -maxdepth 1 -name "*.o")
@@ -65,10 +60,9 @@ EOF
     $LD $USER_LDFLAGS -o "$USER" $USER_OBJS
     objcopy --strip-all "$USER"
   
-    # Quick and dirty, please fix me soon
-    as --32 src/boot/boot.S -o build/boot.o
-    ld -T src/boot/link.ld build/boot.o -o build/boot.bin
-    cat build/boot.bin build/kernel.elf build/user.elf >> cinux.img
+    $NASM -f elf32 src/boot/boot.asm -o build/boot.o
+    $LD -T src/boot/link.ld build/boot.o -o build/boot.bin
+    cat build/boot.bin build/kernel.elf build/user.elf > cinux.img
     
     rm -rf "$BUILD_DIR"
 }
