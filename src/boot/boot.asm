3e0109668c57c1628b4c4c9f647c0e3c37032310
Connor Thomson <blumatrikz@gmail.com>
2025-12-17 19:00:07 -0800

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,41 +1,61 @@
 [BITS 16]
 
-STACK_TOP equ 0x7C00
-LOAD_ADDR equ 0x9000
-LOAD_SEGMENTS equ 0x900
-SECT_SIZE equ 512
+KERNEL_LOAD_ADDR equ 0x1000
+KERNEL_SECTORS equ 16
 
 start:
     cli
-    xor cx, cx
-    mov ss, cx
-    mov sp, STACK_TOP
-    mov ds, cx
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
 
-    mov ax, LOAD_SEGMENTS
+load_kernel:
+    mov ax, KERNEL_LOAD_ADDR
     mov es, ax
     xor bx, bx
+    
     mov ah, 0x02
-    mov al, 64
-    mov cx, 0x0002
-    xor dh, dh
+    mov al, KERNEL_SECTORS
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0
     int 0x13
+    jc load_error
 
+enable_a20:
     in al, 0x92
     or al, 2
     out 0x92, al
 
+enter_protected_mode:
     cli
     lgdt [gdt_descriptor]
-
+    
     mov eax, cr0
     or eax, 1
     mov cr0, eax
+    
+    jmp 0x08:protected_mode
 
-    jmp 0x08:prot32
+load_error:
+    mov si, error_msg
+.loop:
+    lodsb
+    test al, al
+    jz .halt
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop
+.halt:
+    cli
+    hlt
 
 [BITS 32]
-prot32:
+protected_mode:
     mov ax, 0x10
     mov ds, ax
     mov es, ax
@@ -43,14 +63,16 @@ prot32:
     mov gs, ax
     mov ss, ax
     mov esp, 0x90000
-
-    jmp LOAD_ADDR
+    
+    jmp KERNEL_LOAD_ADDR * 0x10
 
 [BITS 16]
+error_msg db "Load error!", 0
 
 gdt_start:
     dq 0
 
+gdt_code:
     dw 0xFFFF
     dw 0x0000
     db 0x00
@@ -58,6 +80,7 @@ gdt_start:
     db 11001111b
     db 0x00
 
+gdt_data:
     dw 0xFFFF
     dw 0x0000
     db 0x00
