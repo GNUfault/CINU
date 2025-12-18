f8ea724b0e68bfd7c3bb84ed0233fb9abc375692
Connor Thomson <blumatrikz@gmail.com>
2025-12-17 17:44:16 -0800

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,30 +1,9 @@
 [BITS 16]
 
 STACK_TOP equ 0x7C00
-ELF_HDR_LOAD equ 0x9000
+LOAD_ADDR equ 0x9000
+LOAD_SEGMENTS equ 0x900
 SECT_SIZE equ 512
-SECT_SHIFT equ 9
-GEOM_SECTORS equ 18
-GEOM_HEADS equ 2
-
-elf_phoff equ 0x1C
-elf_phnum equ 0x2C
-elf_entry equ 0x18
-
-elf_seg_type equ 0x00
-elf_seg_file_offset equ 0x04
-elf_seg_paddr equ 0x0C
-elf_seg_filesz equ 0x10
-elf_seg_struct_size equ 0x20
-
-gdt_fl_pglimit equ 0x80
-gdt_fl_32b equ 0x40
-gdt_a_present equ 0x80
-gdt_a_dpl0 equ 0x00
-gdt_a_nosys equ 0x10
-gdt_a_exec equ 0x08
-gdt_a_rw equ 0x2
-gdt_a_accessed equ 0x1
 
 start:
     cli
@@ -33,142 +12,64 @@ start:
     mov sp, STACK_TOP
     mov ds, cx
 
-    mov ax, 0x0201
+    mov ax, LOAD_SEGMENTS
+    mov es, ax
+    xor bx, bx
+    mov ah, 0x02
+    mov al, 64
     mov cx, 0x0002
     xor dh, dh
-    mov bx, ELF_HDR_LOAD/16
-    mov es, bx
-    xor bx, bx
     int 0x13
-
-load_init:
-    mov ax, ELF_HDR_LOAD/16
-    mov ds, ax
-    mov si, [elf_phoff]
-    mov cl, [elf_phnum]
-
-load_segment:
-    push cx
-    mov eax, [si + elf_seg_type]
-    dec eax
-    jnz skip_seg
-
-    mov ebx, [si + elf_seg_paddr]
-    shr ebx, 4
-    mov es, bx
-
-    mov eax, [si + elf_seg_file_offset]
-    shr eax, SECT_SHIFT
-    inc ax
-
-    push dx
-    xor dx, dx
-    mov bx, GEOM_SECTORS
-    div bx
-    mov cl, dl
-    inc cl
-    mov bl, GEOM_HEADS
-    div bl
-    pop dx
-    mov dh, ah
-    mov ch, al
-
-    xor ebx, ebx
-    mov bx, SECT_SIZE - 1
-    mov eax, [si + elf_seg_filesz]
-    add eax, ebx
-    shr eax, SECT_SHIFT
-
-    mov ah, 0x02
-    xor bx, bx
-    int 0x13
-    jc load_seg_fail
-
-skip_seg:
-    pop cx
-    add si, elf_seg_struct_size
-    loop load_segment
-
-    mov esi, [elf_entry]
-
-    cld
-    mov ax, ds
-    mov es, ax
-    xor di, di
-
-    xor eax, eax
-    mov al, 3*8 - 1
-    stosw
-    mov ax, ELF_HDR_LOAD + 8
-    stosd
-    stosw
-
-    xor eax, eax
-    stosd
-    stosd
-
-    mov eax, [cs:gdt_code]
-    mov edx, [cs:gdt_code+4]
-    stosd
-    xchg eax, edx
-    stosd
-    xchg eax, edx
-    stosd
-    xchg eax, edx
-    stosd
-
-    mov al, gdt_a_present | gdt_a_nosys | gdt_a_dpl0 | gdt_a_rw | gdt_a_accessed
-    mov [ds:8*2 + 5], al
 
     in al, 0x92
     or al, 2
     out 0x92, al
 
-    lgdt [0]
+    cli
+    lgdt [gdt_descriptor]
+
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    jmp 8:prot32
+    jmp 0x08:prot32
 
 [BITS 32]
 prot32:
-    xor eax, eax
-    mov al, 16
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
+    mov esp, 0x90000
 
-    jmp esi
+    jmp LOAD_ADDR
 
 [BITS 16]
 
-load_seg_fail:
-    cli
-    hlt
+gdt_start:
+    dq 0
 
-print:
-    cld
-    mov ah, 0x0E
-    xor bx, bx
-.loop:
-    lodsb
-    test al, al
-    jz .ret
-    int 0x10
-    jmp .loop
-.ret:
-    ret
-
-gdt_code:
     dw 0xFFFF
     dw 0x0000
-    db 0x0
-    db gdt_a_present | gdt_a_nosys | gdt_a_dpl0 | gdt_a_exec | gdt_a_rw | gdt_a_accessed
-    db gdt_fl_32b | gdt_fl_pglimit | 0xF
-    db 0x0
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
 
 times 510-($-$$) db 0
 dw 0xAA55
