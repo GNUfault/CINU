534fa46a8ac152959f600b6e1d9ade9a4e4cb42f
Connor Thomson <blumatrikz@gmail.com>
2025-12-17 17:28:02 -0800

    Update boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,219 +1,108 @@
-bits 16
-
-STACK_TOP equ 0x7C00
-SELF_LOAD equ 0x7C00
-ELF_HDR_LOAD equ 0x7E00
-SECT_SIZE equ 512
-SECT_SHIFT equ 9
-GEOM_SECTORS equ 18
-GEOM_CYLINDERS equ 80
-GEOM_HEADS equ 2
-elf_phoff equ 0x1C
-elf_phnum equ 0x2C
-elf_entry equ 0x18
-elf_seg_type equ 0x00
-elf_seg_file_offset equ 0x04
-elf_seg_paddr equ 0x0C
-elf_seg_filesz equ 0x10
-elf_seg_memsz equ 0x14
-elf_seg_struct_size equ 0x20
-gdt_fl_pglimit equ 0x80
-gdt_fl_32b equ 0x40
-gdt_a_present equ 0x80
-gdt_a_dpl0 equ 0x00
-gdt_a_nosys equ 0x10
-gdt_a_exec equ 0x08
-gdt_a_conforming equ 0x04
-gdt_a_grow_down equ 0x04
-gdt_a_rw equ 0x2
-gdt_a_accessed equ 0x1
-
-section .text
-global start
+[BITS 16]
+[ORG 0x7C00]
 
 start:
-    cli
-    jmp 0:start2
-
-start2:
-    xor cx, cx
-    mov ss, cx
-    mov sp, STACK_TOP
-    mov ds, cx
-    mov ax, 0xB800
-    mov es, ax
-    xor di, di
-    mov ah, 0x0F
-    mov al, ' '
-    mov cx, 2000
-
-.clear_loop:
-    stosw
-    loop .clear_loop
-    mov ah, 0x02
-    xor bh, bh
-    xor dh, dh
-    xor dl, dl
-    int 0x10
-    mov si, drives
-
-scan_floppy:
-    lodsb
-    test al, al
-    jz no_drives
-    mov dl, al
-    mov ax, 0x0201
-    mov cx, 0x0002
-    xor dx, dx
-    mov bx, ELF_HDR_LOAD/16
-    mov es, bx
-    xor bx, bx
-    int 0x13
-    jc scan_floppy
-    jmp load_init
-
-load_init:
-    mov ax, ELF_HDR_LOAD/16
+    xor ax, ax
     mov ds, ax
-    mov si, [elf_phoff]
-    mov cl, [elf_phnum]
-
-load_segment:
-    push cx
-    mov eax, [si + elf_seg_type]
-    dec eax
-    jnz skip_seg
-    mov ebx, [si + elf_seg_paddr]
-    shr ebx, 4
-    mov es, bx
-    mov eax, [si + elf_seg_file_offset]
-    shr eax, SECT_SHIFT
-    inc ax
-    push dx
-    xor dx, dx
-    mov bx, GEOM_SECTORS
-    div bx
-    mov cl, dl
-    inc cl
-    mov bl, GEOM_HEADS
-    div bl
-    pop dx
-    mov dh, ah
-    mov ch, al
-    xor ebx, ebx
-    mov bx, SECT_SIZE - 1
-    mov eax, [si + elf_seg_filesz]
-    add eax, ebx
-    shr eax, SECT_SHIFT
-    mov ah, 0x02
-    xor bx, bx
-    int 0x13
-    jc load_seg_fail
-
-skip_seg:
-    pop cx
-    add si, elf_seg_struct_size
-    loop load_segment
-    mov esi, [elf_entry]
-
-gdtp equ 0
-gdt equ 8
-
-    cld
-    mov ax, ds
     mov es, ax
-    xor di, di
-    xor eax, eax
-    mov al, 3*8 - 1
-    stosw
-    mov ax, ELF_HDR_LOAD + gdt
-    stosd
-    stosw
-    xor eax, eax
-    stosd
-    stosd
-    lea bx, [gdt_code]
-    mov eax, [cs:bx]
-    mov edx, [cs:bx+4]
-    stosd
-    xchg eax, edx
-    stosd
-    xchg eax, edx
-    stosd
-    xchg eax, edx
-    stosd
-    mov al, gdt_a_present | gdt_a_nosys | gdt_a_dpl0 | gdt_a_rw | gdt_a_accessed
-    mov byte [ds:gdt + 8*2 + 5], al
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov ax, 0x1000
+    mov es, ax
+    xor bx, bx
+    mov ah, 0x02
+    mov al, 32
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0
+    int 0x13
+    jc disk_error
+
+    mov si, 0x10000
+    cmp dword [si], 0x464C457F
+    jne elf_error
+
+    mov eax, 0x10000
+    add eax, [si + 24]
+    mov [entry_point], eax
+
     in al, 0x92
     or al, 2
     out 0x92, al
-    lgdt [gdtp]
+
+    cli
+    lgdt [gdt_descriptor]
+
     mov eax, cr0
     or eax, 1
     mov cr0, eax
-    jmp 8:prot32
 
-bits 32
+    jmp 0x08:protected_mode
 
-prot32:
-    xor eax, eax
-    mov al, 16
+disk_error:
+    mov si, msg_disk_err
+    call print
+    jmp $
+
+elf_error:
+    mov si, msg_elf_err
+    call print
+    jmp $
+
+print:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print
+.done:
+    ret
+
+msg_disk_err: db "Disk error!", 0
+msg_elf_err: db "Invalid ELF!", 0
+
+entry_point: dd 0
+
+gdt_start:
+    dq 0
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+protected_mode:
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp esi
+    mov esp, 0x90000
 
-bits 16
+    mov eax, [entry_point]
+    jmp eax
 
-print:
-    cld
-    mov ah, 0x0E
-    xor bx, bx
-
-.loop:
-    lodsb
-    test al, al
-    jz .ret
-    int 0x10
-    jmp .loop
-
-.ret:
-    ret
-
-no_drives:
-    mov ds, bx
-    mov si, msg_no_drives
-    call print
-    cli
     hlt
-
-drives:
-    db 0x00
-    db 0x01
-    db 0x00
-
-load_seg_fail:
-    mov ds, bx
-    mov si, msg_load_seg_fail
-    call print
-    cli
-    hlt
-
-msg_no_drives:
-    db "Error: could not find any bootable devices!"
-
-msg_load_seg_fail:
-    db "Error: failed to load one or more segemnts!"
-
-gdt_code:
-    dw 0xFFFF
-    dw 0x0000
-    db 0x0
-    db gdt_a_present | gdt_a_nosys | gdt_a_dpl0 | gdt_a_exec | gdt_a_rw | gdt_a_accessed
-    db gdt_fl_32b | gdt_fl_pglimit | 0xF
-    db 0x0
 
 times 510-($-$$) db 0
-dw 0xaa55
+dw 0xAA55
