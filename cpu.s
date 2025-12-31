8c388f686c68b0c4476224a0e38182d87223c64d
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 12:36:59 -0800

    Create cpu.s

---
+++ cpu.s
@@ -0,0 +1,25 @@
+/*
+ * CINUX
+ * Copyright (C) 2025 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+.intel_syntax noprefix  # Use intel syntax
+.global halt            # Export halt to linker
+                        #
+halt:                   # halt();
+    cli                 # Disable interrupts
+.loop:                  # Loop label
+    hlt                 # Halt CPU
+    jmp .loop           # Halt again if CPU unhalts
