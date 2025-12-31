c9b50b0ee5fe8e897fb77140d16a5ddebbfbaf88
Connor Thomson <blumatrikz@gmail.com>
2025-12-30 19:17:25 -0800

    Update vga.c

--- vga.c
+++ vga.c
@@ -1,88 +1,196 @@
-.intel_syntax noprefix
-.section .multiboot
-.align 4
+#include <stdint.h>
 
-multiboot_header:
-    .long 0x1BADB002
-    .long 0x00000003
-    .long -(0x1BADB002 + 0x00000003)
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+#define FONT_WIDTH 8
+#define FONT_HEIGHT 16
+#define COLOR_LIGHT_GREY 0xC0C0C0
 
-.section .text
-.global _start
+static uint32_t *framebuffer = NULL;
+static uint8_t font_data[256 * FONT_HEIGHT];
+static int cursor_x = 0;
+static int cursor_y = 0;
 
-_start:
-    cli
+void load_font(void) {
+    extern uint8_t _binary_font_bin_start[];
+    uint8_t *font_ptr = _binary_font_bin_start;
     
-    mov ax, 0x4F02
-    mov bx, 0x4118
-    int 0x10
-    
-    cmp ax, 0x004F
-    jne vbe_fail
-    
-    mov ax, 0x4F01
-    mov cx, 0x4118
-    mov di, offset vbe_mode_info
-    int 0x10
-    
-    lgdt [gdt_descriptor]
-    
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
-    
-    jmp 0x08:protected_mode
+    for (int i = 0; i < 256 * FONT_HEIGHT; i++) {
+        font_data[i] = font_ptr[i];
+    }
+}
 
-vbe_fail:
-    hlt
-    jmp vbe_fail
+static void draw_char(char c, int x, int y, uint32_t color) {
+    uint8_t *glyph = &font_data[(unsigned char)c * FONT_HEIGHT];
+    
+    for (int row = 0; row < FONT_HEIGHT; row++) {
+        uint8_t line = glyph[row];
+        for (int col = 0; col < FONT_WIDTH; col++) {
+            if (line & (0x80 >> col)) {
+                int px = x + col;
+                int py = y + row;
+                if (px < SCREEN_WIDTH && py < SCREEN_HEIGHT) {
+                    framebuffer[py * SCREEN_WIDTH + px] = color;
+                }
+            }
+        }
+    }
+}
 
-protected_mode:
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+static void scroll_screen(void) {
+    for (int y = FONT_HEIGHT; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            framebuffer[(y - FONT_HEIGHT) * SCREEN_WIDTH + x] = 
+                framebuffer[y * SCREEN_WIDTH + x];
+        }
+    }
     
-    mov esp, 0x90000
-    
-    mov eax, [vbe_mode_info + 40]
-    push eax
-    
-    call kmain
-    
-hang:
-    hlt
-    jmp hang
+    for (int y = SCREEN_HEIGHT - FONT_HEIGHT; y < SCREEN_HEIGHT; y++) {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
+            framebuffer[y * SCREEN_WIDTH + x] = 0;
+        }
+    }
+}
 
-.align 16
-gdt_start:
-    .quad 0x0000000000000000
+static void putchar_internal(char c) {
+    if (c == '\n') {
+        cursor_x = 0;
+        cursor_y += FONT_HEIGHT;
+    } else if (c == '\r') {
+        cursor_x = 0;
+    } else if (c == '\t') {
+        cursor_x = (cursor_x + FONT_WIDTH * 4) & ~(FONT_WIDTH * 4 - 1);
+    } else if (c == '\b') {
+        if (cursor_x >= FONT_WIDTH) {
+            cursor_x -= FONT_WIDTH;
+        }
+    } else {
+        draw_char(c, cursor_x, cursor_y, COLOR_LIGHT_GREY);
+        cursor_x += FONT_WIDTH;
+    }
     
-gdt_code:
-    .word 0xFFFF
-    .word 0x0000
-    .byte 0x00
-    .byte 0b10011010
-    .byte 0b11001111
-    .byte 0x00
+    if (cursor_x >= SCREEN_WIDTH) {
+        cursor_x = 0;
+        cursor_y += FONT_HEIGHT;
+    }
     
-gdt_data:
-    .word 0xFFFF
-    .word 0x0000
-    .byte 0x00
-    .byte 0b10010010
-    .byte 0b11001111
-    .byte 0x00
+    if (cursor_y >= SCREEN_HEIGHT) {
+        scroll_screen();
+        cursor_y = SCREEN_HEIGHT - FONT_HEIGHT;
+    }
+}
 
-gdt_end:
+static void print_int(int num) {
+    if (num == 0) {
+        putchar_internal('0');
+        return;
+    }
+    
+    char buffer[12];
+    int i = 0;
+    int is_negative = 0;
+    
+    if (num < 0) {
+        is_negative = 1;
+        num = -num;
+    }
+    
+    while (num > 0) {
+        buffer[i++] = '0' + (num % 10);
+        num /= 10;
+    }
+    
+    if (is_negative) {
+        putchar_internal('-');
+    }
+    
+    while (i > 0) {
+        putchar_internal(buffer[--i]);
+    }
+}
 
-gdt_descriptor:
-    .word gdt_end - gdt_start - 1
-    .long gdt_start
+static void print_hex(unsigned int num) {
+    const char *hex_digits = "0123456789ABCDEF";
+    putchar_internal('0');
+    putchar_internal('x');
+    
+    for (int i = 28; i >= 0; i -= 4) {
+        putchar_internal(hex_digits[(num >> i) & 0xF]);
+    }
+}
 
-.section .bss
-.align 256
-vbe_mode_info:
-    .skip 256
+static inline void va_start_impl(char **ap, void *last) {
+    *ap = (char *)last + ((sizeof(void *) + 3) & ~3);
+}
+
+static inline void va_end_impl(char **ap) {
+    *ap = (void *)0;
+}
+
+static inline void *va_arg_impl(char **ap, int size) {
+    void *result = (void *)*ap;
+    *ap += (size + 3) & ~3;
+    return result;
+}
+
+void printk(const char *format, ...) {
+    char *args;
+    va_start_impl(&args, (void *)&format);
+    
+    while (*format) {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'd':
+                case 'i': {
+                    int *p = va_arg_impl(&args, sizeof(int));
+                    print_int(*p);
+                    break;
+                }
+                case 'x':
+                case 'X': {
+                    unsigned int *p = va_arg_impl(&args, sizeof(unsigned int));
+                    print_hex(*p);
+                    break;
+                }
+                case 's': {
+                    const char **p = va_arg_impl(&args, sizeof(const char *));
+                    const char *str = *p;
+                    while (*str) {
+                        putchar_internal(*str++);
+                    }
+                    break;
+                }
+                case 'c': {
+                    int *p = va_arg_impl(&args, sizeof(int));
+                    putchar_internal((char)*p);
+                    break;
+                }
+                case '%':
+                    putchar_internal('%');
+                    break;
+                default:
+                    putchar_internal('%');
+                    putchar_internal(*format);
+                    break;
+            }
+        } else {
+            putchar_internal(*format);
+        }
+        format++;
+    }
+    
+    va_end_impl(&args);
+}
+
+void vga_init(uint32_t *fb_address) {
+    framebuffer = fb_address;
+    cursor_x = 0;
+    cursor_y = 0;
+    
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        framebuffer[i] = 0;
+    }
+    
+    load_font();
+}
