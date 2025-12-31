9964df019d2ac0bb96e52c998f3b02906ee196a4
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 13:37:36 -0800

    Create idt.h

---
+++ idt.h
@@ -0,0 +1,24 @@
+/*
+ * CINUX
+ * Copyright (C) 2025 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef IDT_H
+#define IDT_H
+
+void idt_init(void);
+void idt_set_gate(int num, unsigned int handler, unsigned short sel, unsigned char flags);
+
+#endif
