8a8be8a3d7df5aa782ac3526b70511f1a8527585
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 15:26:31 -0800

    Update io.s

--- io.s
+++ io.s
@@ -19,20 +19,26 @@
 
 .global outb
 outb:
+    mov dx, [esp+4]
+    mov al, [esp+8]
     out dx, al
     ret
 
 .global inb
 inb:
+    mov dx, [esp+4]
     in al, dx
     ret
 
 .global inw
 inw:
+    mov dx, [esp+4]
     in ax, dx
     ret
 
 .global outw
 outw:
+    mov dx, [esp+4]
+    mov ax, [esp+8]
     out dx, ax
     ret
