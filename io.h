40818e26b4eddc13dd14419a5c2b4f5bd75b805c
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 12:40:30 -0800

    Create io.h

---
+++ io.h
@@ -0,0 +1,26 @@
+/*
+ * CINUX
+ * Copyright (C) 2025 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef IO_H
+#define IO_H
+
+void outb(unsigned short port, unsigned char val);
+unsigned char inb(unsigned short port);
+unsigned short inw(unsigned short port);
+void outw(unsigned short port, unsigned short val);
+
+#endif
