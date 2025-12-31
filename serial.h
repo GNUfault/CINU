8209f850e01f71fd3f657ad625baf94ebfbca8aa
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 13:38:21 -0800

    Create serial.h

---
+++ serial.h
@@ -0,0 +1,27 @@
+/*
+ * CINUX
+ * Copyright (C) 2025 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SERIAL_H
+#define SERIAL_H
+
+void serial_init(void);
+void serial_putchar(char c);
+void serial_write(const char *str);
+char serial_getchar(void);
+int serial_received(void);
+
+#endif
