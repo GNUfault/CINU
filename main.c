ebecc50f4503100da81ecb89eb132eea1268495b
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 15:20:55 -0800

    Update main.c

--- main.c
+++ main.c
@@ -20,9 +20,14 @@
 #include "idt.h"
 #include "serial.h"
 
+#include "cpu.h"
+
 void kmain(unsigned int *framebuffer) {
     vga_init(framebuffer);
     idt_init();
+    
+    halt();
+    
     serial_init();
     log("hi\n");
 
