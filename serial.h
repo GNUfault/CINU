5372f88a94b5fb8f0542fa037b26a38510e15f7c
Connor Thomson <blumatrikz@gmail.com>
2025-12-31 20:57:01 -0800

    Update serial.h

--- serial.h
+++ serial.h
@@ -19,9 +19,6 @@
 #define SERIAL_H
 
 void serial_init(void);
-void serial_putchar(char c);
-void serial_write(const char *str);
-char serial_getchar(void);
-int serial_received(void);
+void serial_console_write(const char *s);
 
 #endif
