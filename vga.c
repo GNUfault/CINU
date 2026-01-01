4b8238eb8c016e18afee7317db7961670d32e064
Connor Thomson <blumatrikz@gmail.com>
2026-01-01 13:20:45 -0800

    Update vga.c

--- vga.c
+++ vga.c
@@ -19,12 +19,12 @@
 
 #define VGA_WIDTH 128
 #define VGA_HEIGHT 48
-#define VGA_MEM ((unsigned char*)0x5000)
 #define FONT_HEIGHT 16
 #define FONT_WIDTH 8
 
 unsigned int cursor_x = 0;
 unsigned int cursor_y = 0;
+static unsigned char* framebuffer;
 
 extern unsigned char _binary_font_bin_start[];
 extern unsigned char _binary_font_bin_end[];
@@ -32,15 +32,18 @@ extern unsigned char _binary_font_bin_size[];
 
 #define font _binary_font_bin_start
 
+void store_fb_address(unsigned int *fb) {
+    framebuffer = (unsigned char*)fb;
+}
+
 static void scroll_screen(void) {
     int i;
-    unsigned char* vga = VGA_MEM;
-    unsigned int* src = (unsigned int*)(vga + FONT_HEIGHT * VGA_WIDTH * 3);
-    unsigned int* dst = (unsigned int*)vga;
+    unsigned int* src = (unsigned int*)(framebuffer + FONT_HEIGHT * VGA_WIDTH * 3);
+    unsigned int* dst = (unsigned int*)framebuffer;
     for (i = 0; i < (VGA_HEIGHT - 1) * FONT_HEIGHT * VGA_WIDTH * 3 / 4; i++) {
         dst[i] = src[i];
     }
-    unsigned int* clear = (unsigned int*)(vga + (VGA_HEIGHT - 1) * FONT_HEIGHT * VGA_WIDTH * 3);
+    unsigned int* clear = (unsigned int*)(framebuffer + (VGA_HEIGHT - 1) * FONT_HEIGHT * VGA_WIDTH * 3);
     for (i = 0; i < FONT_HEIGHT * VGA_WIDTH * 3 / 4; i++) {
         clear[i] = 0;
     }
@@ -49,7 +52,6 @@ static void scroll_screen(void) {
 
 void printk(const char* str) {
     serial_console_write(str);
-    unsigned char* vga = VGA_MEM;
     while (*str) {
         char c = *str++;
         if (c == '\n') {
@@ -64,7 +66,7 @@ void printk(const char* str) {
         int row, col;
         for (row = 0; row < FONT_HEIGHT; row++) {
             unsigned char bits = glyph[row];
-            unsigned int* pixel = (unsigned int*)(vga + (cursor_y * FONT_HEIGHT + row) * VGA_WIDTH * 3 + cursor_x * 3);
+            unsigned int* pixel = (unsigned int*)(framebuffer + (cursor_y * FONT_HEIGHT + row) * VGA_WIDTH * 3 + cursor_x * 3);
             for (col = 0; col < FONT_WIDTH; col++) {
                 if (bits & 0x80) {
                     *pixel = 0xC0C0C0;
