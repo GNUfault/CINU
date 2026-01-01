cb4a9de84eee9ff80b230d29962f4c5032fc732b
Connor Thomson <blumatrikz@gmail.com>
2026-01-01 13:48:19 -0800

    Create vga.h

---
+++ vga.h
@@ -0,0 +1,28 @@
+/*
+ * CINUX
+ * Copyright (C) 2025 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef VGA_H
+#define VGA_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void scroll_screen(void);
+void vga_putchar(char c);
+void print(const char *str);
+
+#endif
