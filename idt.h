0dc2023672f4305a9fcb41d519695f7a8b044bb8
Connor Thomson <blumatrikz@gmail.com>
2026-01-02 14:08:52 -0800

    Update idt.h

--- idt.h
+++ idt.h
@@ -15,9 +15,26 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef GDT_H
-#define GDT_H
+#ifndef IDT_H
+#define IDT_H
 
-void gdt_init(void);
+// IDT flags
+#define IDT_PRESENT 0x80
+#define IDT_DPL0 0x00
+#define IDT_DPL1 0x20
+#define IDT_DPL2 0x40
+#define IDT_DPL3 0x60
+#define IDT_INT_GATE 0x0E
+#define IDT_TRAP_GATE 0x0F
+
+// Common gate types
+#define IDT_GATE_INT_RING0  (IDT_PRESENT | IDT_DPL0 | IDT_INT_GATE)
+#define IDT_GATE_INT_RING3  (IDT_PRESENT | IDT_DPL3 | IDT_INT_GATE)
+#define IDT_GATE_TRAP_RING0 (IDT_PRESENT | IDT_DPL0 | IDT_TRAP_GATE)
+#define IDT_GATE_TRAP_RING3 (IDT_PRESENT | IDT_DPL3 | IDT_TRAP_GATE)
+
+void idt_init(void);
+void idt_set_gate(unsigned char num, unsigned int base, 
+                  unsigned short selector, unsigned char flags);
 
 #endif
