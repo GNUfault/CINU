d8b08bf18b1b4b0759de7a2d0592ae6a968c2efc
Connor Thomson <blumatrikz@gmail.com>
2026-01-02 14:11:38 -0800

    Update gdt_s.s

--- gdt_s.s
+++ gdt_s.s
@@ -19,18 +19,19 @@
 
 .global gdt_flush
 gdt_flush:
-    mov eax, [esp+4]    # Get the pointer to the GDT pointer
-    lgdt [eax]          # Load the GDT
+    mov eax, [esp+4] # Get the pointer to the GDT pointer
+    lgdt [eax]       # Load the GDT
 
-    mov ax, 0x10        # 0x10 is the offset to our data segment
-    mov ds, ax          # Load all data segment selectors
+    mov ax, 0x10 # 0x10 is the offset to our data segment
+    mov ds, ax   # Load all data segment selectors
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
 
-    jmp 0x08:flush_cs   # 0x08 is the offset to our code segment
-                        # Far jump to flush the instruction pipeline
+    # 0x08 is the offset to our code segment
+    # Far jump to flush the instruction pipeline
+    jmp 0x08:flush_cs 
 
 flush_cs:
     ret
