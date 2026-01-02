a93280ab813f55b8e74d97cb2578efc97bb11dcf
Connor Thomson <blumatrikz@gmail.com>
2026-01-02 13:05:14 -0800

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -15,4 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# You really need a whole shell file just to execute one thing?
 autoreconf --install
