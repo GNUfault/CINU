1689c27de39ec2a164a44f5dace115cd24ece433
Connor Thomson <blumatrikz@gmail.com>
2026-01-02 17:51:30 -0800

    Update vga.c

--- vga.c
+++ vga.c
@@ -15,65 +15,72 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#define VESA_FB_ADDR_PTR 0x5000
-#define VESA_WIDTH  1024
+#include "vga.h"
+
+#define VESA_WIDTH 1024
 #define VESA_HEIGHT 768
-#define VESA_PITCH  (1024 * 4) /* bytes per row */
-#define FONT_WIDTH  8
+#define VESA_PITCH (1024*4)
+#define FONT_WIDTH 8
 #define FONT_HEIGHT 16
 
-static unsigned int *vesa_fb = 0;
+static unsigned int *vesa_fb = (unsigned int *)(*((unsigned int *)0x5000));
 extern unsigned char _binary_font_bin_start[];
 
-void store_fb_address(void) {
-    unsigned int *ptr = (unsigned int *)VESA_FB_ADDR_PTR;
-    vesa_fb = (unsigned int *)(*ptr);
+static inline void vga_putpixel(unsigned int x, unsigned int y, unsigned int color) {
+    vesa_fb[y*(VESA_PITCH>>2)+x] = color;
 }
 
-static inline void vga_putpixel(unsigned int x, unsigned int y, unsigned int color ) {
-    vesa_fb[y * (VESA_PITCH >> 2) + x] = color;
-}
-
-void vga_clear(unsigned int color) {
-    unsigned int i = 0;
-    unsigned int pixels = VESA_WIDTH * VESA_HEIGHT;
-
-    while (i < pixels) {
-        vesa_fb[i] = color;
-        i++;
+static void scroll_up() {
+    unsigned int x,y;
+    for(y=0;y<VESA_HEIGHT-FONT_HEIGHT;y++) {
+        for(x=0;x<VESA_WIDTH;x++) {
+            unsigned int *fb=(unsigned int *)((char *)vesa_fb + (y*VESA_WIDTH+x)*4);
+            unsigned int *src=(unsigned int *)((char *)vesa_fb + ((y+FONT_HEIGHT)*VESA_WIDTH+x)*4);
+            *fb = *src;
+        }
+    }
+    for(y=VESA_HEIGHT-FONT_HEIGHT;y<VESA_HEIGHT;y++) {
+        for(x=0;x<VESA_WIDTH;x++) {
+            unsigned int *fb=(unsigned int *)((char *)vesa_fb + (y*VESA_WIDTH+x)*4);
+            *fb=0x000000;
+        }
     }
 }
 
-void vga_draw_char(unsigned int x, unsigned int y, char c, unsigned int fg, unsigned int bg) {
-    unsigned int row = 0;
-    unsigned char *glyph =
-        _binary_font_bin_start + ((unsigned char)c * FONT_HEIGHT);
-
-    while (row < FONT_HEIGHT) {
-        unsigned char bits = glyph[row];
-        unsigned int col = 0;
-
-        while (col < FONT_WIDTH) {
-            if (bits & (0x80 >> col))
-                vga_putpixel(x + col, y + row, fg);
-            else
-                vga_putpixel(x + col, y + row, bg);
+static void vga_draw_char_internal(unsigned int x, unsigned int y, char c, unsigned int fg, unsigned int bg) {
+    unsigned int row=0;
+    unsigned char *glyph=_binary_font_bin_start + ((unsigned char)c*FONT_HEIGHT);
+    while(row<FONT_HEIGHT) {
+        unsigned char bits=glyph[row];
+        unsigned int col=0;
+        while(col<FONT_WIDTH) {
+            if(bits & (0x80>>col)) vga_putpixel(x+col,y+row,fg);
+            else vga_putpixel(x+col,y+row,bg);
             col++;
         }
         row++;
     }
 }
 
-void vga_draw_string(unsigned int x, unsigned int y, const char *s, unsigned int fg, unsigned int bg) {
-    unsigned int cx = x;
+static unsigned int cursor_x=0;
+static unsigned int cursor_y=0;
+#define PRINTK_FG 0xC0C0C0
+#define PRINTK_BG 0x000000
 
-    while (*s) {
-        if (*s == '\n') {
-            cx = x;
-            y += FONT_HEIGHT;
+void printk(const char *s) {
+    while(*s) {
+        if(*s=='\n') {
+            cursor_x=0;
+            cursor_y+=FONT_HEIGHT;
+            if(cursor_y+FONT_HEIGHT>VESA_HEIGHT) scroll_up();
         } else {
-            vga_draw_char(cx, y, *s, fg, bg);
-            cx += FONT_WIDTH;
+            vga_draw_char_internal(cursor_x,cursor_y,*s,PRINTK_FG,PRINTK_BG);
+            cursor_x+=FONT_WIDTH;
+            if(cursor_x+FONT_WIDTH>VESA_WIDTH) {
+                cursor_x=0;
+                cursor_y+=FONT_HEIGHT;
+                if(cursor_y+FONT_HEIGHT>VESA_HEIGHT) scroll_up();
+            }
         }
         s++;
     }
