e776096f2926b46922dc8380cf15bce00543b5cb
Connor Thomson <blumatrikz@gmail.com>
2026-01-03 12:54:45 -0800

    Create string.h

---
+++ string.h
@@ -0,0 +1,24 @@
+/*
+ * CINUX
+ * Copyright (C) 2025-2026 Connor Thomson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef STRING_H
+#define STRING_H
+
+void kmemmove(unsigned char *dst, const unsigned char *src, unsigned int n);
+void kmemset(unsigned char *dst, unsigned char val, unsigned int n);
+
+#endif
