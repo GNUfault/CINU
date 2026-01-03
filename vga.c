495ea826668392982c4af170671cb6fc90fea6ae
Connor Thomson <blumatrikz@gmail.com>
2026-01-02 19:30:53 -0800

    Update vga.c

--- vga.c
+++ vga.c
@@ -30,21 +30,101 @@ static inline void vga_putpixel(unsigned int x, unsigned int y, unsigned int col
     vesa_fb[y*(VESA_PITCH>>2)+x] = color;
 }
 
+// NTSC Color Bars Test Pattern
+void test_pattern(void) {
+    unsigned int x, y;
+    unsigned int bar_width = VESA_WIDTH / 7;
+    
+    // NTSC Color Bar colors (in RGB format)
+    unsigned int colors[7] = {
+        0xFFFFFF,  // White
+        0xFFFF00,  // Yellow
+        0x00FFFF,  // Cyan
+        0x00FF00,  // Green
+        0xFF00FF,  // Magenta
+        0xFF0000,  // Red
+        0x0000FF   // Blue
+    };
+    
+    // Draw the main color bars (top 2/3 of screen)
+    unsigned int main_height = (VESA_HEIGHT * 2) / 3;
+    for(y = 0; y < main_height; y++) {
+        for(x = 0; x < VESA_WIDTH; x++) {
+            unsigned int bar = x / bar_width;
+            if(bar > 6) bar = 6;
+            vga_putpixel(x, y, colors[bar]);
+        }
+    }
+    
+    // Draw the bottom section with smaller blocks
+    unsigned int bottom_start = main_height;
+    unsigned int bottom_height = VESA_HEIGHT - main_height;
+    unsigned int block_height = bottom_height / 3;
+    
+    // Middle section: Reverse order bars
+    for(y = bottom_start; y < bottom_start + block_height; y++) {
+        for(x = 0; x < VESA_WIDTH; x++) {
+            unsigned int bar = x / bar_width;
+            if(bar > 6) bar = 6;
+            vga_putpixel(x, y, colors[6 - bar]);
+        }
+    }
+    
+    // Bottom left: Black to white gradient
+    unsigned int gradient_start = bottom_start + block_height;
+    for(y = gradient_start; y < gradient_start + block_height; y++) {
+        for(x = 0; x < VESA_WIDTH / 2; x++) {
+            unsigned int shade = (x * 255) / (VESA_WIDTH / 2);
+            unsigned int color = (shade << 16) | (shade << 8) | shade;
+            vga_putpixel(x, y, color);
+        }
+    }
+    
+    // Bottom right: Black
+    for(y = gradient_start; y < gradient_start + block_height; y++) {
+        for(x = VESA_WIDTH / 2; x < VESA_WIDTH; x++) {
+            vga_putpixel(x, y, 0x000000);
+        }
+    }
+    
+    // Very bottom: PLUGE pattern (black, <3.5%, black, 3.5%, black, >3.5%, black)
+    unsigned int pluge_start = gradient_start + block_height;
+    unsigned int pluge_width = VESA_WIDTH / 7;
+    unsigned int pluge_colors[7] = {
+        0x000000,  // Black
+        0x050505,  // Below black (<3.5%)
+        0x000000,  // Black
+        0x090909,  // 3.5% white
+        0x000000,  // Black  
+        0x0D0D0D,  // Above black (>3.5%)
+        0x000000   // Black
+    };
+    
+    for(y = pluge_start; y < VESA_HEIGHT; y++) {
+        for(x = 0; x < VESA_WIDTH; x++) {
+            unsigned int section = x / pluge_width;
+            if(section > 6) section = 6;
+            vga_putpixel(x, y, pluge_colors[section]);
+        }
+    }
+}
+
 static void scroll_up() {
     unsigned int x,y;
+    // Copy rows up by FONT_HEIGHT pixels
     for(y=0;y<VESA_HEIGHT-FONT_HEIGHT;y++) {
         for(x=0;x<VESA_WIDTH;x++) {
-            unsigned int *fb=(unsigned int *)((char *)vesa_fb + (y*VESA_WIDTH+x)*4);
-            unsigned int *src=(unsigned int *)((char *)vesa_fb + ((y+FONT_HEIGHT)*VESA_WIDTH+x)*4);
-            *fb = *src;
+            vesa_fb[y*(VESA_PITCH>>2)+x] = vesa_fb[(y+FONT_HEIGHT)*(VESA_PITCH>>2)+x];
         }
     }
+    // Clear the bottom FONT_HEIGHT rows
     for(y=VESA_HEIGHT-FONT_HEIGHT;y<VESA_HEIGHT;y++) {
         for(x=0;x<VESA_WIDTH;x++) {
-            unsigned int *fb=(unsigned int *)((char *)vesa_fb + (y*VESA_WIDTH+x)*4);
-            *fb=0x000000;
+            vesa_fb[y*(VESA_PITCH>>2)+x] = 0x000000;
         }
     }
+    // Adjust cursor position
+    cursor_y -= FONT_HEIGHT;
 }
 
 static void vga_draw_char_internal(unsigned int x, unsigned int y, char c, unsigned int fg, unsigned int bg) {
@@ -72,14 +152,18 @@ void printk(const char *s) {
         if(*s=='\n') {
             cursor_x=0;
             cursor_y+=FONT_HEIGHT;
-            if(cursor_y+FONT_HEIGHT>VESA_HEIGHT) scroll_up();
+            if(cursor_y >= VESA_HEIGHT - FONT_HEIGHT) {
+                scroll_up();
+            }
         } else {
             vga_draw_char_internal(cursor_x,cursor_y,*s,PRINTK_FG,PRINTK_BG);
             cursor_x+=FONT_WIDTH;
-            if(cursor_x+FONT_WIDTH>VESA_WIDTH) {
+            if(cursor_x >= VESA_WIDTH) {
                 cursor_x=0;
                 cursor_y+=FONT_HEIGHT;
-                if(cursor_y+FONT_HEIGHT>VESA_HEIGHT) scroll_up();
+                if(cursor_y >= VESA_HEIGHT - FONT_HEIGHT) {
+                    scroll_up();
+                }
             }
         }
         s++;
